6d61ba49b9727109860da5fee7a06a0dad57f351
Shermin Sultana <rima64549@gmail.com>
2023-07-04 23:27:25 +0600

    case

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = "blackPink";
+const userInput = "blackPinK";
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+if( userName.toLowerCase() === userInput.toLowerCase()){
+    console.log("case is valid");
+}
+else{
+    console.log("case is invalid");
+}
