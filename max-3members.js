258fb5de5a5fe8a3b39871769cdd6776c94cebcb
Shermin Sultana <rima64549@gmail.com>
2023-07-06 02:28:34 +0600

    max

---
+++ max-3members.js
@@ -0,0 +1,24 @@
+const jim = 84;
+const dela = 99;
+const chinku = 77;
+if(jim>dela && jim>chinku){
+    console.log("jim will get the delicious cake");
+}
+else if(dela>jim && dela>chinku){
+    console.log("dela will get the delicious cake");
+}
+else{
+    console.log("chinku will get the delicious cake");
+}
+const jim1 = 69;
+const dela1 = 97;
+const chinku1 = 99;
+if(jim1>dela1 && jim1>chinku1){
+    console.log("jim will get the delicious cake");
+}
+else if(dela1>jim1 && dela1>chinku1){
+    console.log("dela will get the delicious cake");
+}
+else{
+    console.log("chinku will get the delicious cake");
+}
