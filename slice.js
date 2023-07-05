eb7ac6dcc408c280b3e6f8180db93d51e9ce3a73
Shermin Sultana <rima64549@gmail.com>
2023-07-05 20:38:45 +0600

    split

---
+++ slice.js
@@ -0,0 +1,2 @@
+const lyrics = "Branching is used in version control and software management to maintain stability while isolated changes are made to code.";
+const pa
---
+++ split.js
@@ -0,0 +1,8 @@
+
+const lyrics = "Branching is used in. version control and software .management to maintain stability while isolated changes are made to code."
+console.log(lyrics);
+const parts = lyrics.split(" ");
+console.log(parts);
+
+console.log(lyrics.split("."));
+console.log(lyrics.split(""));
