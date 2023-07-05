68f65acd0b285cc0866fdc2bf69dd484bd87e749
Shermin Sultana <rima64549@gmail.com>
2023-07-05 20:51:38 +0600

    trim

---
+++ trim.js
@@ -0,0 +1,6 @@
+const greetings = "   HELLO WORLD   "
+console.log(greetings.trim());
+const greetings2 = "   HELLO WORLD , I AM COMING, HOW IS YOUR DAY  ";
+console.log(greetings2.trimStart());
+const greetings3 = "   HELLO WORLD , I AM COMING, HOW IS YOUR DAY  ";
+console.log(greetings3.trimEnd());
