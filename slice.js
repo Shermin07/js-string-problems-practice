f765e90bec452c94047d8994e68751cc11c48c5a
Shermin Sultana <rima64549@gmail.com>
2023-07-05 20:43:14 +0600

    slice substing

--- slice.js
+++ slice.js
@@ -1,2 +1,7 @@
 const lyrics = "Branching is used in version control and software management to maintain stability while isolated changes are made to code.";
-const pa
+const partial = lyrics.slice(5, 10);
+console.log(partial)
+const partial2 = lyrics.slice(5, 20);
+console.log(partial2)
+
+// substring wll be the same
