f1b8a37daf69e76a1e551664fb2d34bb0beae2c2
Shermin Sultana <rima64549@gmail.com>
2023-07-07 15:44:59 +0600

    add

--- reverse.js
+++ reverse.js
@@ -1,5 +1,5 @@
 function reverseString(text){
-    for(i = text.length-1; i>=0; i--){
+    for(i = 0; i< text.length; i++){
         const element = text[i];
         console.log(i, element);
 
@@ -7,3 +7,25 @@ function reverseString(text){
 }
 const myString = "i am a good boy"
 const reversed = reverseString(myString);
+//
+function reverseString1(text){
+    for(i = text.length-1 ; i>=0; i--){
+        const element = text[i];
+        console.log(i, element);
+
+    }
+}
+const myString1 = "i am a good boy"
+const reversed1 = reverseString1(myString1);
+//
+function reverseString2(text){
+    let reversed = " ";
+    for(i = text.length-1 ; i>=0; i--){
+        const element = text[i];
+        reversed = reversed + element;
+        console.log(reversed, element);
+
+    }
+}
+const myString2 = "i am a good boy"
+const reversed2 = reverseString2(myString2);
